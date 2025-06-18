b40e5242e0c93b05d4f2bcfdd38e774a52f0e8c1
sakhezech <contact@sakhezech.ru>
2025-06-18 14:25:25 +0300

    refactor: remove renaming projects

--- src/listok/__init__.py
+++ src/listok/__init__.py
@@ -17,8 +17,8 @@ def get_todos(config: Config) -> dict[str, dict[str, str]]:
         if not file.exists():
             continue
 
+        project_name = project_dir.name
         project_todos: dict[str, list[str]] = {}
-        project_name: str | None = None
         curr_todo: list[str] | None = None
 
         lines = file.read_text().splitlines()
@@ -28,12 +28,9 @@ def get_todos(config: Config) -> dict[str, dict[str, str]]:
                 curr_todo = []
                 title = line.removeprefix('##').strip()
                 project_todos[title] = curr_todo
-            elif line.startswith('#'):
-                project_name = line.removeprefix('#').strip()
             elif curr_todo is not None:
                 curr_todo.append(line)
 
-        project_name = project_name if project_name else project_dir.name
         collected[project_name] = {
             k: '\n'.join(v).strip() for k, v in project_todos.items()
         }
